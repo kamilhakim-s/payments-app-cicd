2bd82aec72d4f47bfd5f09575ac40463ccabf57c
Kamilhakim S. <90536611+kamilhakim-s@users.noreply.github.com>
2025-09-01 10:29:41 +0100

    Feature User Service CI Pipeline (#1)
    * Add CI pipeline and configuration files for UserService
    
    * Update CI actions to use latest versions and enhance .gitignore for local documentation
    
    * Refactor user service code for improved readability and maintainability; standardize string formatting and enhance error handling
    
    * Refactor code for improved readability; update security scan to exclude specific directories and enhance user service validation messages
    
    * Refactor code for improved readability
    
    * Update CI configuration to use Python 3.11 and replace safety with pip-audit for dependency vulnerability checks
    
    * Update Docker actions to use version 3 for setup-buildx and login
    
    * Optimize Docker image tagging strategy and push conditions in CI workflow

--- src/accounts/userservice/tests/constants.py
+++ src/accounts/userservice/tests/constants.py
@@ -18,76 +18,96 @@ Example constants used in tests
 import random
 import string
 from datetime import datetime
-from Crypto.PublicKey import RSA
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 
 
 def generate_rsa_key():
     """Generate priv,pub key pair for test"""
-    key = RSA.generate(2048)
-    private_key = key.export_key()
-    public_key = key.publickey().export_key()
-    return private_key, public_key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+    # Serialize private key
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+
+    # Serialize public key
+    public_key = private_key.public_key()
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+
+    # Return as decoded strings (not bytes) for compatibility with JWT library
+    return private_pem.decode("utf-8"), public_pem.decode("utf-8")
+
 
 def get_random_string(length):
     """Generate random string of given length"""
-    return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+    return "".join(random.choice(string.ascii_lowercase) for _ in range(length))
+
 
 EXAMPLE_PRIVATE_KEY, EXAMPLE_PUBLIC_KEY = generate_rsa_key()
 
-TIMESTAMP_FORMAT = '%Y-%m-%d'
+TIMESTAMP_FORMAT = "%Y-%m-%d"
 EXAMPLE_USER_REQUEST = {
-    'username': 'jdoe',
-    'password': 'pwd',
-    'password-repeat': 'pwd',
-    'firstname': 'John',
-    'lastname': 'Doe',
-    'birthday': '2000-01-01',
-    'timezone': 'GMT+1',
-    'address': '1600 Amphitheatre Parkway',
-    'state': 'CA',
-    'zip': '94043',
-    'ssn': '123',
+    "username": "jdoe",
+    "password": "pwd",
+    "password-repeat": "pwd",
+    "firstname": "John",
+    "lastname": "Doe",
+    "birthday": "2000-01-01",
+    "timezone": "GMT+1",
+    "address": "1600 Amphitheatre Parkway",
+    "state": "CA",
+    "zip": "94043",
+    "ssn": "123",
 }
 EXAMPLE_USER = {
-    'accountid': '123',
-    'username': 'jdoe',
-    'passhash': b'hjfsrf#jrsfj',
-    'firstname': 'John',
-    'lastname': 'Doe',
-    'birthday': datetime.strptime('2000-01-01', TIMESTAMP_FORMAT).date(),
-    'timezone': 'GMT+1',
-    'address': '1600 Amphitheatre Parkway',
-    'state': 'CA',
-    'zip': '94043',
-    'ssn': '123',
+    "accountid": "123",
+    "username": "jdoe",
+    "passhash": b"hjfsrf#jrsfj",
+    "firstname": "John",
+    "lastname": "Doe",
+    "birthday": datetime.strptime("2000-01-01", TIMESTAMP_FORMAT).date(),
+    "timezone": "GMT+1",
+    "address": "1600 Amphitheatre Parkway",
+    "state": "CA",
+    "zip": "94043",
+    "ssn": "123",
 }
 EXPECTED_FIELDS = [
-    'username',
-    'password',
-    'password-repeat',
-    'firstname',
-    'lastname',
-    'birthday',
-    'timezone',
-    'address',
-    'state',
-    'zip',
-    'ssn',
+    "username",
+    "password",
+    "password-repeat",
+    "firstname",
+    "lastname",
+    "birthday",
+    "timezone",
+    "address",
+    "state",
+    "zip",
+    "ssn",
 ]
 
 # Usernames must be >1 and <=15 chars, alphanumeric and underscores
 INVALID_USERNAMES = [
-    None, # null
-    "", # empty string
-    " ", # only space
-    "b", # single character
-    " user", # starting with space
-    "*$&%($", # non alphanumeric characters
-    "user*new", # alphanumeric with non alphanumeric characters
-    "🏦💸", # emojis
-    "user1💸", # alphanumeric with emojis
-    get_random_string(16), # 16 characters
-    " {}".format(get_random_string(15)), # 15 characters + leading space
-    "{} ".format(get_random_string(15)), # 15 characters + trailing space
-    "{}".format(get_random_string(100)), # 100 characters
-    ]
+    None,  # null
+    "",  # empty string
+    " ",  # only space
+    "b",  # single character
+    " user",  # starting with space
+    "*$&%($",  # non alphanumeric characters
+    "user*new",  # alphanumeric with non alphanumeric characters
+    "🏦💸",  # emojis
+    "user1💸",  # alphanumeric with emojis
+    get_random_string(16),  # 16 characters
+    " {}".format(get_random_string(15)),  # 15 characters + leading space
+    "{} ".format(get_random_string(15)),  # 15 characters + trailing space
+    "{}".format(get_random_string(100)),  # 100 characters
+]
--- src/accounts/userservice/tests/test_db.py
+++ src/accounts/userservice/tests/test_db.py
@@ -33,7 +33,7 @@ class TestDb(unittest.TestCase):
     def setUp(self):
         """Init db and create table before each test"""
         # init SQLAlchemy with sqllite in mem
-        self.db = UserDb('sqlite:///:memory:')
+        self.db = UserDb("sqlite:///:memory:")
         # create users table in mem
         self.db.users_table.create(self.db.engine)
 
@@ -41,8 +41,8 @@ class TestDb(unittest.TestCase):
         """test if a user can be added"""
         user = EXAMPLE_USER.copy()
         # create a user with username foo
-        user['username'] = 'foo'
-        user['accountid'] = '1'
+        user["username"] = "foo"
+        user["accountid"] = "1"
         # add user to db
         self.db.add_user(user)
 
@@ -50,8 +50,8 @@ class TestDb(unittest.TestCase):
         """test if one user can be added twice"""
         user = EXAMPLE_USER.copy()
         # create a user with username bar
-        user['username'] = 'bar'
-        user['accountid'] = '2'
+        user["username"] = "bar"
+        user["accountid"] = "2"
         # add bar_user to db
         self.db.add_user(user)
         # try to add same user again
@@ -61,31 +61,31 @@ class TestDb(unittest.TestCase):
         """test getting a user"""
         user = EXAMPLE_USER.copy()
         # create a user with username baz
-        user['username'] = 'baz'
-        user['accountid'] = '3'
+        user["username"] = "baz"
+        user["accountid"] = "3"
         # add baz_user to db
         self.db.add_user(user)
         # get baz_user from db
-        db_user = self.db.get_user(user['username'])
+        db_user = self.db.get_user(user["username"])
         # assert both user objects are equal
         self.assertEqual(user, db_user)
 
     def test_get_non_existent_user_returns_none(self):
         """test getting a user that does not exist"""
         # assert None when user does not exist
-        self.assertIsNone(self.db.get_user('user1'))
+        self.assertIsNone(self.db.get_user("user1"))
 
     # mock random.randint to produce 4,5,6 on each invocation
-    @patch('random.randint', side_effect=[4, 5, 6])
+    @patch("random.randint", side_effect=[4, 5, 6])
     def test_generate_account_id_ignores_existing_id_generates_new_id(self, mock_rand):
         """test generating account id"""
         user = EXAMPLE_USER.copy()
         # create a user with username qux
-        user['username'] = 'qux'
-        user['accountid'] = '4'
+        user["username"] = "qux"
+        user["accountid"] = "4"
         # add qux_user to db
         # generate_account_id should return 5 now as 4 exists
         self.db.add_user(user)
-        self.assertEqual('5', self.db.generate_accountid())
+        self.assertEqual("5", self.db.generate_accountid())
         # mock_rand was called twice, first generating 4, then 5
         self.assertEqual(2, mock_rand.call_count)
